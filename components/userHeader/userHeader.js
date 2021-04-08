181860cd6a05ccb50565c9d581535e902b4a05e1
YUN <1620175472@qq.com>
2021-04-08 18:27:03 +0800

    告白墙以及评论详情页
    告白墙以及评论详情页

---
+++ components/userHeader/userHeader.js
@@ -0,0 +1,34 @@
+// utils/components/userHeader.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    postTime:{
+      type:String,
+      value:''
+    },
+    userName:{
+      type:String,
+      value:''
+    },
+    avatarUrl:{
+      type:String,
+      value:''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
