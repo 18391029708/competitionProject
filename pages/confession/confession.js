4f028b26665e4e7c559502b0c7a64825f2d8ccc2
YUN <1620175472@qq.com>
2021-05-26 10:14:50 +0800

    圈子修改
    圈子修改

--- pages/confession/confession.js
+++ pages/confession/confession.js
@@ -6,98 +6,96 @@ Page({
    * 页面的初始数据
    */
   data: {
-    loading:true,
+    loading: true,
     isLike: false,
-    confessions:[],
-    userInfo:{},
-    userId:"",
-    bottomRefresh:false,
-    totalConfess:4,
-    curConfessCount:0,
-    showBackTop:false
+    confessions: [],
+    userInfo: {},
+    userId: "",
+    bottomRefresh: false,
+    totalConfess: 4,
+    curConfessCount: 0,
+    showBackTop: false,
+    topNum: 0,
+    active: 0
+  },
+  // tab切换
+  onChange(e) {
+    this.setData({
+      active: e.detail.index
+    })
   },
   // 发送评论或者点赞信息
   // 修改点赞列表
-  modify(index,data){
+  modify(index, data) {
     const db = wx.cloud.database();
     db.collection('t_confession').doc(this.data.confessions[index]._id).update({
-      data:{
-        likerArr:data
+      data: {
+        likerArr: data
       },
-      succcess:function(res){
-        console.log("修改点赞成功：",res);
+      succcess: function (res) {
+        console.log("修改点赞成功：", res);
       }
     })
   },
-  // 图片预览
-  previewImg(e){
-    // preindex是confession对应confessions的下标
-    // index是图片对应confessions[preindex].pictures[index]
-    const {index,preindex} = e.currentTarget.dataset;
-    wx.previewImage({
-      current:this.data.confessions[preindex].pictures[index],
-      urls: this.data.confessions[preindex].pictures
-    })
-  },
   // 点赞事件
-  handleLike(e){
+  handleLike(e) {
     console.log(e);
-    let {index} = e.currentTarget.dataset;
+    let { index } = e.currentTarget.dataset;
     // 没未登录
-    if(JSON.stringify(this.data.userInfo) == "{}"||JSON.stringify(this.data.userInfo) === "null"){
+    if (JSON.stringify(this.data.userInfo) == "{}" || JSON.stringify(this.data.userInfo) === "null") {
       wx.showToast({
-        icon:"error",
+        icon: "error",
         title: '登录后可点赞',
       })
-    }else{
+    } else {
       let isExist = this.data.confessions[index].likerArr.indexOf(this.data.userId);
-      const {confessions} = this.data;
-      console.log(isExist,confessions);
-      if(isExist===-1){ // 还未点赞
+      const { confessions } = this.data;
+      console.log(isExist, confessions);
+      if (isExist === -1) { // 还未点赞
         confessions[index].likerArr.push(this.data.userId);
-        confessions[index].likeClass="icon-like"; // 红心
-      }else{ // 取消点赞
+        confessions[index].likeClass = "icon-like"; // 红心
+      } else { // 取消点赞
         console.log('cancel');
-        confessions[index].likerArr.splice(isExist,1);
+        confessions[index].likerArr.splice(isExist, 1);
         confessions[index].likeClass = "icon-like1";
       }
       this.setData({
         confessions
       })
-      this.modify(index,confessions[index].likerArr);
+      this.modify(index, confessions[index].likerArr);
     }
   },
   // 跳转
-  handleNavigate(e){
+  handleNavigate(e) {
     wx.navigateTo({
       url: e.currentTarget.dataset.url,
     })
   },
   // 更新表白墙列表 
-  uploadConfession(){
+  uploadConfession() {
     wx.cloud.callFunction({
-      name:"OperateDatabase",
-      data:{
-        opr:'query',
-        tablename:"t_confession",
-        data:{}
+      name: "OperateDatabase",
+      data: {
+        opr: 'query',
+        tablename: "t_confession",
+        data: {}
       },
-      complete:(res)=>{
+      complete: (res) => {
         let confessions = res.result.data;
         let userId = app.globalData.openid;
         // 根据openId来确定 点赞列表 是否含有该用户
         // 处理当前用户是否喜爱当前表白
-        confessions.map((item,index)=>{
-          if(item.likerArr.indexOf(userId)!==-1){
+        confessions.map((item, index) => {
+          if (item.likerArr.indexOf(userId) !== -1) {
             return item.likeClass = "icon-like";
           }
         })
         this.setData({
-          confessions:confessions.reverse(),
-          loading:false,
-          userInfo:app.globalData.userInfo,
+          confessions: confessions.reverse(),
+          loading: false,
+          userInfo: app.globalData.userInfo,
           userId,
-          curConfessCount:confessions.length
+          curConfessCount: confessions.length
         })
       }
     })
@@ -106,25 +104,36 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
   // 返回顶部
-  handleBackTop(){
-    wx.pageScrollTo({
-      scrollTop:0,
-      duration: 300
-    })
+  handleBackTop() {
+    this.setData({
+      topNum: 0
+    });
   },
   // 显示返回顶部按钮
-  onPageScroll(e){
-    if(e.scrollTop>250){
+  showBackToTop(e) {
+    if (e.scrollTop > 250) {
       this.setData({
-        showBackTop:true
+        showBackTop: true
       })
-    }else{
+    } else {
       this.setData({
-        showBackTop:false
+        showBackTop: false
       })
+    }
+  },
+  // 获取滚动条当前位置
+  scrolltoupper: function (e) {
+    if (e.detail.scrollTop > 100) {
+      this.setData({
+        showBackTop: true
+      });
+    } else {
+      this.setData({
+        showBackTop: false
+      });
     }
   },
   /**
@@ -162,13 +171,10 @@ Page({
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+  scrollReachBottom: function () {
     console.log("chudi");
     this.setData({
-      bottomRefresh:true
+      bottomRefresh: true
     })
   },
 
